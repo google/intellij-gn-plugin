82ef5648ddc0b58eb65e6950feca1bf659424d88
Bruno Dal Bo <brunodalbo@google.com>
2020-03-28 10:53:45 -0700

    Add ParseTest

---
+++ src/test/java/com/google/idea/gn/ParseTest.kt
@@ -0,0 +1,21 @@
+//  Copyright (c) 2020 Google LLC All rights reserved.
+//  Use of this source code is governed by a BSD-style
+//  license that can be found in the LICENSE file.
+
+package com.google.idea.gn
+
+import com.intellij.testFramework.ParsingTestCase
+
+class ParseTest : ParsingTestCase("", "gn", true, GnParserDefinition()) {
+
+  override fun getTestDataPath(): String = "src/test/testData"
+
+  override fun skipSpaces(): Boolean = true
+
+  override fun includeRanges(): Boolean = true
+
+  fun testSimpleParseCheck() {
+    doTest(true)
+  }
+
+}
